380c05600cb6a37111bf39a573ca09b8ce900c03
Dev2@gmail.com <Admin@Rushikesh>
2020-04-10 23:53:04 +0530

    mofify abc.java

--- DemoGit/src/com/nt/beans/Abc.java
+++ DemoGit/src/com/nt/beans/Abc.java
@@ -2,7 +2,13 @@ package com.nt.beans;
 
 public class Abc {
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		
+		int a=10;
+		int b=10;
+		int c;
+		
+		c = a+b;
+		System.out.println(c);
+		
 	}
-
 }
